e9a2353e05c88ebe4ed8d83ff2739aaf14db3368
tejvuligonda <tejvuligonda@gmail.com>
2016-11-04 05:03:45 -0700

    Updated git addcommitpush script to allow for pushing to multiple branches

--- git/addcommitpush.sh
+++ git/addcommitpush.sh
@@ -4,15 +4,50 @@
 #   Add this line: alias doit='bash ~/addcommitpush.sh'                                                 
 #   Save and quit                                                                                       
 #   Run this command: source ~/.bashrc                                                                  
+add_all()
+{
+    git add -A *
+}
+
+commit()
+{
+    git commit -m "$1"
+}
+
+push_first() {
+     remote_info=$(git remote -v)
+     remote_name=$(echo $remote_info | awk '{print $1;}')
+     current_branch_name=$(git rev-parse --abbrev-ref HEAD)
+     # git rev-parse --abbrev-ref HEAD echos the name of the current branch                          
+    git push $remote_name $current_branch_name
+}
+
+pushall() {
+    remote_info=$(git remote)
+    current_branch_name=$(git rev-parse --abbrev-ref HEAD)
+    for remote in $remote_info
+    do 
+        git push $remote $current_branch_name
+    done    
+}
+
 if [[ $1 == "" ]]; then
     echo "No commit message provided"
     exit 1
 else
-    git add .
-    git commit -m "$1"
-    remote_info=$(git remote -v)
-    remote_name=$(echo $remote_info | awk '{print $1;}')
-    current_branch_name=$(git rev-parse --abbrev-ref HEAD)
-    # git rev-parse --abbrev-ref HEAD echos the name of the current branch                              
-    git push $remote_name $current_branch_name
+	if [[ $1 == "-a" ]]; then
+        echo ""
+        if [[ $2 == "" ]]; then
+            echo "No commit message provided"
+            exit 1
+        else
+            add_all
+            commit $2
+            pushall
+        fi
+	else
+        add_all
+        commit $1
+        push_first
+    fi
 fi
