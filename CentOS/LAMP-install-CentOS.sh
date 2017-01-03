065a04f093441e668366322ddfc421c5910b784e
tejvuligonda <tejvuligonda@gmail.com>
2017-01-03 01:31:36 -0500

    Updated CentOS LAMP install script to account for different versions of CentOS

--- CentOS/LAMP-install-CentOS.sh
+++ CentOS/LAMP-install-CentOS.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 sudo yum install -y httpd
-sudo yum install -y mariadb-server
 sudo yum install -y php php-mysql
+version=$(rpm -q --queryformat '%{VERSION}' centos-release)
+SEVEN=7
+if [ $version -lt $SEVEN ]; then
+    sudo yum install -y mysql-server
+else
+    sudo yum install -y mariadb-server
+fi
 sudo service httpd restart
 sudo echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
 php -r 'echo "\n\nYour PHP is working.\n\n";'
