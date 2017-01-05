8be597d6b7173a816b5e833b89cfe3a6a8120598
tejvuligonda <tejvuligonda@gmail.com>
2017-01-04 22:57:58 -0800

    Updated LAMP install script for Ubuntu to fix phpinfo initialization bug

--- Ubuntu/LAMP-install-Ubuntu.sh
+++ Ubuntu/LAMP-install-Ubuntu.sh
@@ -4,5 +4,5 @@ sudo apt-get install -y apache2
 sudo apt-get install -y mysql-server
 sudo apt-get install -y php5 libapache2-mod-php5
 sudo /etc/init.d/apache2 restart
-sudo echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
+sudo bash -c "echo \"<?php phpinfo(); ?>\" >> /var/www/html/info.php"
 php -r 'echo "\n\nYour PHP is working.\n\n";'
