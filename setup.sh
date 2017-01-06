b8f6698d86930fd0449729723d4858ca71807504
tejvuligonda <tejvuligonda@gmail.com>
2017-01-05 23:45:47 -0800

    Added setup script which copies various files and creates shortcuts

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 MY_PATH=$(dirname $0)
 MY_PATH=$(cd $MY_PATH && pwd)
 cp $MY_PATH/git/.gitconfig $MY_PATH/vi/.vimrc ~
-echo "alias acp='$MY_PATH/git/addcommitpush.sh'" >> ~/.bashrc
-echo "alias asu='$MY_PATH/git/auto-setup-git.sh'" >> ~/.bashrc
-source ~/.bashrc
+echo "alias acp='bash $MY_PATH/git/addcommitpush.sh'" >> ~/.bash_aliases
+echo "alias asu='bash $MY_PATH/git/auto-setup-git.sh'" >> ~/.bash_aliases
+. ~/.bash_aliases
+. ~/.bashrc
