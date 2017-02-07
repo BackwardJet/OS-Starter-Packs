38e2bae62ba8a0f1a7cb1b15d4453da03a1aa53b
tejvuligonda <tejvuligonda@gmail.com>
2017-02-07 12:54:46 -0800

    Added VirtualBox Guest additions to install-essentials script for Ubuntu

--- Ubuntu/install-essentials-Ubuntu.sh
+++ Ubuntu/install-essentials-Ubuntu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y vim
-sudo apt-get install -y git
+sudo apt-get install -y vim git
+if ( lspci | grep 'VirtualBox' >/dev/null );
+    then sudo apt-get install -y virtualbox-guest-dkms;
+fi
