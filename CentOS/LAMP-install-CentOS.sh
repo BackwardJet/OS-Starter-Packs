888808873feefb0f536e0367432975b3253fbda4
tejvuligonda <tejvuligonda@gmail.com>
2017-02-21 16:19:09 -0500

    Updated LAMP install script for CentOS to properly install MySQL

--- CentOS/LAMP-install-CentOS.sh
+++ CentOS/LAMP-install-CentOS.sh
@@ -6,9 +6,14 @@ version=$(rpm -q --queryformat '%{VERSION}' centos-release)
 SEVEN=7
 if [ $version -lt $SEVEN ]; then
     sudo yum install -y mysql-server
+    sudo service mysqld start
+    sudo /usr/bin/mysql_secure_installation
 else
-    sudo yum install -y mariadb-server
+    sudo yum install -y mariadb-server mariadb
+    sudo systemctl start mariadb
+    sudo mysql_secure_installation
 fi
+# For MYSQL installation, set the root password and hit enter for all other prompts
 sudo service httpd restart
 sudo echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
 php -r 'echo "\n\nYour PHP is working.\n\n";'
