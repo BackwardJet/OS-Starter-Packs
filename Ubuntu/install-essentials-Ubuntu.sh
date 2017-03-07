eef20018567c26aaa2c6a4054df001d30c105a6b
Tej Vuligonda <tejvuligonda@gmail.com>
2017-03-07 03:55:45 -0800

    Added openssh-server to install-essentials script for Ubuntu VirtualBox VM's

--- Ubuntu/install-essentials-Ubuntu.sh
+++ Ubuntu/install-essentials-Ubuntu.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y vim git
 if ( lspci | grep 'VirtualBox' >/dev/null );
-    then sudo apt-get install -y virtualbox-guest-dkms;
+    then sudo apt-get install -y virtualbox-guest-dkms openssh-server;
 fi
