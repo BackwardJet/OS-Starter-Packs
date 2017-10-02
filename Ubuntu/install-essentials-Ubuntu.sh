77a8c4052104143a3e8753b52432801db03dd2f1
tejvuligonda <tejvuligonda@gmail.com>
2017-10-02 08:56:24 +0000

    Added auto-yes to install-essentials Ubuntu scripts

--- Ubuntu/install-essentials-Ubuntu.sh
+++ Ubuntu/install-essentials-Ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get install -y vim git
 if ( lspci | grep 'VirtualBox' >/dev/null );
     then sudo apt-get install -y virtualbox-guest-dkms openssh-server;
